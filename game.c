2a655a871a6179e528957cec0586559ebfdbef67
hardik singh <realhardik18@gmail.com>
2025-01-15 17:11:29 +0530

    game done

--- game.c
+++ game.c
@@ -4,20 +4,11 @@
 #include <math.h>
 #include <conio.h>
 
-int find_distance(int x1,int y1, int x2, int y2);
+double find_distance(int x1,int y1, int x2, int y2);
 
 int main() {
-    //get name of the user
-    /*char username[100];
-    printf("Welcome to HotOrCold!\n");
-    printf("Enter a user name: ");
-    if (scanf("%99s", username) != 1) {
-        printf("Error reading username\n");
-        return 1;
-    }*/
-    
-    //generating grid
     int GRID_SIZE;
+    int tries = 0;
     printf("Enter the size of GRID to generate: ");
     scanf("%d", &GRID_SIZE);            
 
@@ -27,36 +18,52 @@ int main() {
             grid[x][y] = 0;
         }
     }
-    
-    //generating key cordinates
+        
     srand(time(0));
     int key_x=rand()%GRID_SIZE;
-    int key_y=rand()%GRID_SIZE;
-    grid[key_x][key_y]=1;
-
-    //showing the grid to user
+    int key_y=rand()%GRID_SIZE;    
+    
     for(int x = 0; x < GRID_SIZE; x++) {
         for(int y = 0; y < GRID_SIZE; y++) {
-            printf("\033[38;2;0;128;0m%d \033[0m", grid[x][y]);
+            printf("\033[38;2;0;255;0m%d \033[0m", grid[x][y]);
         }
         printf("\n");
     }
-    printf("THe keycoordinates are: %d %d\n",key_x,key_y);    
-    printf("the distance b/w are: %d",find_distance(0,0,key_x,key_y));
 
-    //exit procedure
-    char exit[100];    
-    printf("game over! enter a key to exit: ");
-    if (scanf("%99s", exit) != 1) {
-        printf("Error reading username\n");
-        return 1;
+    //printf("The keycoordinates are: %d %d\n",key_x,key_y);        
+    int user_x=-1;
+    int user_y=-1;
+    while(user_x!=key_x || user_y!=key_y){
+        grid[user_x][user_y]=0;
+        printf("Enter your coordinates:");
+        scanf("%d %d",&user_x,&user_y);
+        printf("\n");
+        grid[user_x][user_y]=1;
+        for(int x=0;x<GRID_SIZE;x++){
+            for(int y=0;y<GRID_SIZE;y++){
+                if(grid[x][y]==1){
+                    printf("\033[38;2;255;0;0m%d \033[0m",grid[x][y]);                    
+                }
+                else{
+                    printf("\033[38;2;0;255;0m%d \033[0m",grid[x][y]);
+                }
+                
+            }
+            printf("\n");
+        }
+        tries+=1;
+        printf("the distance b/w are: %.3f\n",find_distance(user_x,user_y,key_x,key_y));
+                
     }
-
+    char exit[100];    
+    printf("YOU WIN!!\n");
+    printf("The treasure was at %d %d",key_x,key_y);
+    printf("it took you %d tries to beat the game!",tries);
 }
 
-int find_distance(int x1,int y1, int x2, int y2){
+double find_distance(int x1,int y1, int x2, int y2){
     int x_diff=x2-x1;
     int y_diff=y2-y1;
-    int distance = sqrt((x_diff,2) + pow(y_diff,2));
+    double distance = sqrt(pow(x_diff,2) + pow(y_diff,2));
     return distance;
 }
